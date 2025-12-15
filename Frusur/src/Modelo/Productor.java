941a16b054ed75a4a49394a1eab71e9cca9eeb8e
Jav <javiera.duarte2201@alumnos.ubiobio.cl>
2025-12-14 21:46:13 -0300

    Refactor Productor class with new fields and methods

--- Frusur/src/Modelo/Productor.java
+++ Frusur/src/Modelo/Productor.java
@@ -1,29 +1,26 @@
 package Modelo;
-
 import Utilidades.Rut;
-
 import java.io.Serializable;
 
 public class Productor extends Persona implements Serializable {
-    EstadoProductor estado;
+    private EstadoProductor estado;
+    private TipoBerrie tipoBerrieActual;
+    private double kilosActuales;
+
     public Productor(Rut rut, String nombre, String contacto) {
         super(rut, nombre, contacto);
-        estado = EstadoProductor.PENDIENTE;
+        this.estado = EstadoProductor.RECIBIDO;
+        this.kilosActuales = 0;
     }
 
-    // Retorna true si acepta, false si no. Se asume true para el ejemplo.
-    public boolean aceptarAcuerdo() {
-        estado = EstadoProductor.EN_PROCESO;
-        return true;
-    }
-    
-    public boolean recibirCharla(){
-        estado = EstadoProductor.HABILITADO;
-        return true;
+    public void nuevaSolicitud(TipoBerrie berrie, double kilos) {
+        this.tipoBerrieActual = berrie;
+        this.kilosActuales = kilos;
+        this.estado = EstadoProductor.PENDIENTE;
     }
 
-    public MateriaPrima llevarMateriaPrima(String tipoFruta, double kilos) {
-        // Crea la materia prima asociándose a sí mismo como origen
-        return new MateriaPrima(tipoFruta, kilos, this);
-    }
+    public EstadoProductor getEstado() { return estado; }
+    public void setEstado(EstadoProductor estado) { this.estado = estado; }
+    public TipoBerrie getTipoBerrieActual() { return tipoBerrieActual; }
+    public double getKilosActuales() { return kilosActuales; }
 }
